bd72373818162c6059260f3f648a686f7c10d404
Declan Arkins <declan.arkins@zalando.ie>
2017-05-11 14:54:22 +0100

    adding more of the survey section 2

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,13 +3,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="copy">
-        <dl>
-          <dt>Ashoka Ireland</dt>
-          <dd>23 South William Street, Dublin 2, Ireland | Tel: (+353) 01 532 62 33 | Email: fkoch@ashoka.org</dd>
-          <dd>ASHOKA IS A REGISTERED 501(C)(3) NON-PROFIT ORGANIZATION. © 2017</dd>
-        </dl>
-      </p>
+      <dl className="copy">
+        <dt>Ashoka Ireland</dt>
+        <dd>23 South William Street, Dublin 2, Ireland | Tel: (+353) 01 532 62 33 | Email: fkoch@ashoka.org</dd>
+        <dd>ASHOKA IS A REGISTERED 501(C)(3) NON-PROFIT ORGANIZATION. © 2017</dd>
+      </dl>
     </footer>
   );
 };
--- client/components/SurveyPage.js
+++ client/components/SurveyPage.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Form, Input, Progress, Checkbox, Button, Select, DatePicker } from 'antd';
+import { Form, Input, InputNumber, Progress, Checkbox, Button, Select, Radio } from 'antd';
 const FormItem = Form.Item;
+const RadioGroup = Radio.Group;
 import client from '../api/client';
 
 const formItemLayout = {
   labelCol: {
     xs: {span: 24},
-    sm: {span: 5},
+    sm: {span: 8},
   },
   wrapperCol: {
     xs: {span: 24},
@@ -14,9 +15,21 @@ const formItemLayout = {
   },
 };
 
-const MonthPicker = DatePicker.MonthPicker;
-
 class Home extends Component {
+  constructor(props, context){
+    super(props, context);
+
+    this.state = {
+      organisation: ''
+    };
+  }
+
+  onSelectOrg = (value) => {
+    this.setState({
+      organisation: value
+    });
+  }
+
   submit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -30,6 +43,7 @@ class Home extends Component {
 
   render() {
     const {getFieldDecorator} = this.props.form;
+    const currentYear = new Date().getFullYear();
     return (
       <div className="home-page">
         <p className="text-center">
@@ -146,9 +160,9 @@ class Home extends Component {
 
             <FormItem
               {...formItemLayout}
-              label="Date of Birth"
+              label="Year of Birth"
             >
-              <MonthPicker />
+              <InputNumber defaultValue={currentYear} min={1900} max={currentYear} />
             </FormItem>
 
             <FormItem
@@ -620,6 +634,165 @@ class Home extends Component {
                 )
               }
             </FormItem>
+
+            <FormItem
+              {...formItemLayout}
+              label="Do you work from an office or home?"
+            >
+              <Select placeholder="Select place of work">
+                <Option value="office">Office</Option>
+                <Option value="home">Home</Option>
+              </Select>
+            </FormItem>
+
+            <FormItem
+              {...formItemLayout}
+              label="(If office) Office Location"
+            >
+              <Select placeholder="Select location of office">
+                <Option value="Antrim">Antrim</Option>
+                <Option value="Armagh">Armagh</Option>
+                <Option value="Carlow">Carlow</Option>
+                <Option value="Cavan">Cavan</Option>
+                <Option value="Clare">Clare</Option>
+                <Option value="Cork">Cork</Option>
+                <Option value="Derry">Derry</Option>
+                <Option value="Donegal">Donegal</Option>
+                <Option value="Down">Down</Option>
+                <Option value="Dublin">Dublin</Option>
+                <Option value="Fermanagh">Fermanagh</Option>
+                <Option value="Galway">Galway</Option>
+                <Option value="Kerry">Kerry</Option>
+                <Option value="Kildare">Kildare</Option>
+                <Option value="Kilkenny">Kilkenny</Option>
+                <Option value="Laois">Laois</Option>
+                <Option value="Leitrim">Leitrim</Option>
+                <Option value="Limerick">Limerick</Option>
+                <Option value="Longford">Longford</Option>
+                <Option value="Louth">Louth</Option>
+                <Option value="Mayo">Mayo</Option>
+                <Option value="Meath">Meath</Option>
+                <Option value="Monaghan">Monaghan</Option>
+                <Option value="Offaly">Offaly</Option>
+                <Option value="Roscommon">Roscommon</Option>
+                <Option value="Sligo">Sligo</Option>
+                <Option value="Tipperary">Tipperary</Option>
+                <Option value="Tyrone">Tyrone</Option>
+                <Option value="Waterford">Waterford</Option>
+                <Option value="Westmeath">Westmeath</Option>
+                <Option value="Wexford">Wexford</Option>
+                <Option value="Wicklow">Wicklow</Option>
+              </Select>
+            </FormItem>
+
+            <h2>Section 2 - About Your Work</h2>
+            <hr className="divider secondary" />
+
+            <FormItem
+              label="You've been nominated as a Changemaker in Ireland. Which organisation/s are you involved in to deliver social change?"
+            >
+              <Select placeholder="Select an organisation" onChange={this.onSelectOrg}>
+                <Option value="Organisation 1" key="Organisation 1">Oranistaion 1</Option>
+                <Option value="Organisation 2" key="Organisation 2">Organisation 2</Option>
+                <Option value="Organisation 3" key="Organisation 3">Organisation 3</Option>
+                <Option value="Organisation 4" key="Organisation 4">Organisation 4</Option>
+              </Select>
+            </FormItem>
+
+            <FormItem
+              {...formItemLayout}
+              label={`Are you a founder of (${this.state.organisation})?`}
+            >
+            <RadioGroup>
+              <Radio value="Yes">Yes</Radio>
+              <Radio value="No">No</Radio>
+            </RadioGroup>
+            </FormItem>
+
+            <FormItem
+              {...formItemLayout}
+              label={`What is your job title in (${this.state.organisation})?`}
+            >
+            <Input placeholder="Job Title" />
+
+          </FormItem>
+
+          <FormItem
+            {...formItemLayout}
+            label={`Year ${this.state.organisation} was founded`}
+          >
+            <InputNumber defaultValue={currentYear} min={1900} max={currentYear} />
+          </FormItem>
+
+          <FormItem
+            label={`Which of these sectors does (${this.state.organisation}) operate in?`}
+          >
+            <Select placeholder="Select a sector">
+              <Option value="Protecting & Preserving the Planet">Protecting & Preserving the Planet</Option>
+              <Option value="Social Inclusion">Social Inclusion</Option>
+              <Option value="Physical & Mental Well-being">Physical & Mental Well-being</Option>
+              <Option value="Educating & Developing Skills">Educating & Developing Skills</Option>
+              <Option value="Human Rights">Human Rights</Option>
+            </Select>
+          </FormItem>
+
+          <FormItem
+            label="(If multi) Which would you say is your primary sector in which you operate?"
+          >
+            <Select placeholder="Select a sector">
+              <Option value="Protecting & Preserving the Planet">Protecting & Preserving the Planet</Option>
+              <Option value="Social Inclusion">Social Inclusion</Option>
+              <Option value="Physical & Mental Well-being">Physical & Mental Well-being</Option>
+              <Option value="Educating & Developing Skills">Educating & Developing Skills</Option>
+              <Option value="Human Rights">Human Rights</Option>
+            </Select>
+          </FormItem>
+
+          <FormItem
+            label="How would you describe what you do in short (1-2 sentences)?"
+          >
+            <Input type="textarea" rows={4} />
+          </FormItem>
+
+          <FormItem
+            label="Why did you start this work (1-2 sentences) ?"
+          >
+            <Input type="textarea" rows={4} />
+          </FormItem>
+
+          <FormItem
+            {...formItemLayout}
+            label="Current number of full time paid staff"
+          >
+            <InputNumber defaultValue="0" />
+          </FormItem>
+
+          <FormItem
+            {...formItemLayout}
+            label="Current number of prestart time paid staff"
+          >
+            <InputNumber defaultValue="0" />
+          </FormItem>
+
+          <FormItem
+            {...formItemLayout}
+            label="Current number of volunteers (per year)"
+          >
+            <InputNumber defaultValue="0" />
+          </FormItem>
+
+          <FormItem
+            label={`Which of these sources of founding does (${this.state.organisation}) have?`}
+          >
+            <Select placeholder="Select a sector">
+              <Option value="Earned Revenue">Earned Revenue</Option>
+              <Option value="Public Funding">Public Funding</Option>
+              <Option value="Philanthropy">Philanthropy</Option>
+            </Select>
+          </FormItem>
+
+          <hr className="divider secondary" />
+
           <Button type="primary"
                   htmlType="submit"
                   onClick={this.submit}
